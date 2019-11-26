55457b6492e84e8667c5be009065ca1a250241c9
Equinoz <julien.lecoz33@gmail.com>
2019-11-26 17:12:29 +0100

    Calcul de la première colonne

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 $(function() {
 
+	// Fonction vérifiant si la valeur du champ de saisie est correcte
+	function checkValue(value) {
+		if (value < 0 || value > 50)
+			return false;
+		else
+			return true;
+	}
+
 	// Modification du contenu des cellules du tableau
 	$(".clickable").on("click", function(e) {
 		let cell = $(e.target);
 		let cellValue = cell.text();
 		cell.html("");
-		cell.append("<input type='text' size=3 maxlength=2 />");
+		cell.append("<input type='number' min=0 max=50 style='width:60px' />");
 		let input = cell.children();
 		input.val(cellValue);
 		input.focus();
@@ -16,11 +24,62 @@ $(function() {
 		});
 
 		input.on("blur", function() {
-			cell.text(input.val());
+			if (checkValue(input.val()))
+				cell.text(input.val());
+			else
+				cell.text(cellValue);
 			input.remove();
+			$("#grid").trigger("change");
 		});
 	});
 
+	$("#grid").on("change", function() {
+		let rempli = true;
+		let total = 0;
+		let bonus;
+		$("#first-section tr .column-1.clickable").each(function() {
+			total += Number($(this).text());
+			if ($(this).text() === "") {
+				rempli = false;
+			}
+		});
+		if (rempli) {
+			if (total > 56 && total < 63)
+				bonus = 20;
+			else if (total > 62)
+				bonus = 35;
+			else
+				bonus = 0;
+		}
+		else {
+			total = bonus = "";
+		}
+		$("#first-section #total .column-1").text(total);
+		$("#first-section #bonus .column-1").text(bonus);
+		$("#first-section #first-total .column-1").text(total + bonus);
+
+		/////
+		rempli = true;
+		total = 0;
+		$("#second-section tr .column-1.clickable").each(function() {
+			total += Number($(this).text());
+			if ($(this).text() === "") {
+				rempli = false;
+			}
+		});
+		if (rempli) {
+			$("#second-section #second-total .column-1").text(total);
+		}
+		else
+			$("#second-section #second-total .column-1").text("");
+
+		/////
+
+		if ($("#second-section #second-total .column-1").text() !== "" && $("#first-section #first-total .column-1").text() !== "")
+			$("#total-section #main-total .column-1").text(Number($("#first-section #first-total .column-1").text()) + Number($("#second-section #second-total .column-1").text()));
+		else
+			$("#total-section #main-total .column-1").text("");
+	})
 
 	// Remise à zéro de la grille
 	$("#reset-button").on("click", function() {
